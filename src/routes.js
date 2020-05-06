34c41f7c417ee5abfb7b5ed325e6e55a7c4b9783
IgorVVieira <igor.gutoo63@gmail.com>
2020-05-06 11:15:43 -0300

    CRUD finalizado

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
 
-routes.post('/create', ProductController.store);
+routes.get('/products/:id', ProductController.show);
+
+routes.post('/products', ProductController.store);
+
+routes.put('/products/:id', ProductController.update);
+
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
