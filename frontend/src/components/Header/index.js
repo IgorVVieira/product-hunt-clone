15b45b17487b5b985bba5b24d970210f4cd73c8b
IgorVVieira <igor.gutoo63@gmail.com>
2020-05-08 10:44:59 -0300

    Header finalizado

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+const Header = () => {
+    return (
+        <header id="main-header">
+            JSHunt
+        </header>
+    );
+}
+
+export default Header;
