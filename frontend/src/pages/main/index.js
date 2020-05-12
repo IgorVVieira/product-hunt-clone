891922cc8254c4dd8e1f9f4579744875b5b516ee
IgorVVieira <igor.gutoo63@gmail.com>
2020-05-11 21:19:43 -0300

    Adicionando mudança de páginas anterior/próxima

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -9,23 +9,49 @@ class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     };
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
 
         this.setState({
-            products: response.data.docs
+            products: docs,
+            productInfo,
+            page,
         });
     };
 
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) {
+            return;
+        }
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) {
+            return;
+        }
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    }
+
     render() {
 
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -36,6 +62,10 @@ class Main extends Component {
                         <a href="">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         )
     };
