f3dcaa72e036f700490637e4a5f751af5f0105d3
IgorVVieira <igor.gutoo63@gmail.com>
2020-05-13 11:52:12 -0300

    Adicionando navegação por detalhes do produto

--- frontend/src/pages/product/index.js
+++ frontend/src/pages/product/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './style.css';
 
 class Product extends Component {
+    state = {
+        product: {},
+    };
+
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+
+        const response = api.get(`/products/${id}`);
+        this.setState({ product: (await response).data });
+    }
+
     render() {
+        const { product } = this.state;
+
         return (
-            <h1>Product</h1>
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>
+            </div>
         );
     }
 }
